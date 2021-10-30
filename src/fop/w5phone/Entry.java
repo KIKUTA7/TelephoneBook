f06495174776244397eaccdfcc161567f8d9eddf
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-10-30 13:30:44 +0400

    1/3 unda gaataros wesit

--- src/fop/w5phone/Entry.java
+++ src/fop/w5phone/Entry.java
@@ -1,2 +1,24 @@
 package fop.w5phone;
 
+public class Entry {
+    String firstName, lastName, phoneNumber;
+
+    public Entry(String first, String last, String num)
+    {
+        firstName = first;
+        lastName = last;
+        phoneNumber = num;
+    }
+    public String getFirstName (Entry a)
+    {
+        return a.firstName;
+    }
+    public String getLastNameName (Entry a)
+    {
+        return a.lastName;
+    }
+    public String getPhoneNumber (Entry a)
+    {
+        return a.phoneNumber;
+    }
+}
