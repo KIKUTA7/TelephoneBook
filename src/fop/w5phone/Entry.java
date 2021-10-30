1ec5ff059add712d5b18aa4a55446fc35687837e
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-10-30 14:36:49 +0400

    oo moica raaa

--- src/fop/w5phone/Entry.java
+++ src/fop/w5phone/Entry.java
@@ -1,7 +1,7 @@
 package fop.w5phone;
 
 public class Entry {
-    public String firstName, lastName, phoneNumber;
+    public static String firstName, lastName, phoneNumber;
 
     public Entry(String first, String last, String num)
     {
