22a0f616f11d2be5f92e29b50baa834bd3673279
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-11-01 17:25:25 +0400

    interesis variantia.

--- src/fop/w5phone/PhoneBook.java
+++ src/fop/w5phone/PhoneBook.java
@@ -34,6 +34,31 @@ public class PhoneBook {
 //        return out;
 //
 //    }
+
+//    public String find(String firstname, String lastname) {
+//        String out = null;
+//        int start = 0;
+//        int end = entries.length - 1;
+//        while (start <= end) {
+//            int middle = (start + end) / 2;
+//            int compare = lastname.compareTo(entries[middle].getLastName());
+//            if (compare == 0) {
+//                compare = firstname.compareTo(entries[middle].getFirstName());
+    // anu raxan firstname is compsac igive shedarebebi unda uqna tu lastnamebi udris , mashin firstname is compp ze gadacvli aq
+    // da mere qvemot sheadareb , anu qveda if ebs miewodeba an lastnamebis comp an tu lastnamebi tolia
+    // firstnamebis comp da am yvelafris mere out shi sul daimaxsovrebs middle is phonenumber s .
+//            }  if (compare < 0) {
+//                end = middle - 1;
+//
+//            } if( compare > 0) {
+//                start = middle + 1;
+//            }
+//            out = entries[middle].getPhoneNumber();
+//        }
+//        return out;
+//
+//    }
+
     public String find(String firstname, String lastname) {
         String out = null;
         int start = 0;
@@ -43,15 +68,20 @@ public class PhoneBook {
             int compare = lastname.compareTo(entries[middle].getLastName());
             if (compare == 0) {
                 compare = firstname.compareTo(entries[middle].getFirstName());
+//                if (compare == 0) {
+//                    return  entries[middle].getPhoneNumber();
+//
+//                }  aqac mosula mara wvalebaa aq if .
+
             }  if (compare < 0) {
                 end = middle - 1;
 
-            } if( compare > 0) {
+            } if(compare > 0) {
                 start = middle + 1;
             }
-            out = entries[middle].getPhoneNumber();
+            return entries[middle].getPhoneNumber();
         }
-        return out;
+        return null;
 
     }
 
