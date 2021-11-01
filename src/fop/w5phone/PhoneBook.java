3a60e755c2c5070103fe98088a55b0120e45f6db
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-11-01 16:37:39 +0400

    kai exla ar ginda gawitlebebi da igi. Du willst nicht rot werden. Do not make it red.

--- src/fop/w5phone/PhoneBook.java
+++ src/fop/w5phone/PhoneBook.java
@@ -2,44 +2,39 @@ package fop.w5phone;
 
 public class PhoneBook {
 
-    public Entry [] entries;
+    public Entry[] entries;
 
-    public Entry[] getEntries() {
-        return entries;
+    public PhoneBook (Entry[] a) {
+        entries = a;
     }
-    public String find (String firstname, String lastname)
-    {
-        String out =null;
-        int start = 0 ;
-        int end = entries.length - 1;
-        while (start <= end)
-        {
-            int middle = (start+end)/2;
-            int compare = lastname.compareTo(entries[middle].getLastName());
-            if(compare == 0)
-            {
-                compare = firstname.compareTo(entries[middle].getFirstName());
-                if(compare == 0) {
-                   out =  entries[middle].getPhoneNumber();
-                    break;
-                }
-                else if(compare < 0)
-                    end = middle -1;
-                else
-                    start = middle +1;
-            }
-            else if(compare < 0)
-            {
-                end = middle -1 ;
 
-            }
-            else {
-                start = middle +1;
+    public String find(String firstname, String lastname) {
+        String out = null;
+        int start = 0;
+        int end = entries.length - 1;
+        while (start <= end) {
+            int middle = (start + end) / 2;
+            int compare = lastname.compareTo(entries[middle].getLastName());
+            if (compare == 0) {
+                compare = firstname.compareTo(entries[middle].getFirstName());
+                if (compare == 0) {
+                    out = entries[middle].getPhoneNumber();
+                    break;
+                } else if (compare < 0)
+                    end = middle - 1;
+                else
+                    start = middle + 1;
+            } else if (compare < 0) {
+                end = middle - 1;
+
+            } else {
+                start = middle + 1;
             }
         }
         return out;
 
     }
+}
 //    public int find0(int start, int end,String firstname,String lastname)
 //    {
 //        if(lastname.compareTo(entries[(start+end)/2].getLastName()) < 0)
@@ -47,4 +42,3 @@ public class PhoneBook {
 //
 //        }
 //    }
-}
