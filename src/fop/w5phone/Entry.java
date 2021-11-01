cce6f82e2c0a608ddc1975c5b12d017ae2fd3e26
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-11-01 16:35:01 +0400

    kai exla ar ginda gawitlebebi da igi. Du willst nicht rot werden. Do not make it red.

--- src/fop/w5phone/Entry.java
+++ src/fop/w5phone/Entry.java
@@ -1,24 +1,25 @@
 package fop.w5phone;
 
 public class Entry {
-    private  String firstName, lastName, phoneNumber;
+    private String firstName, lastName, phoneNumber;
 
-    public Entry(String first, String last, String num)
-    {
-      if(first != "")  firstName = first;
-      if(last != "")  lastName = last;
-      if(num != "")  phoneNumber = num;
+    public Entry(String first, String last, String num) {
+         this.firstName = first;
+         this.lastName = last;
+         this.phoneNumber = num;
     }
-    public String getFirstName ()
-    {
+
+    public String getFirstName() {
         return firstName;
     }
-    public String getLastName ()
-    {
+
+    public String getLastName() {
         return lastName;
     }
-    public String getPhoneNumber ()
-    {
+
+    public String getPhoneNumber() {
         return phoneNumber;
     }
+
+
 }
